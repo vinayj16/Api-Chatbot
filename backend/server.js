16256fc4fc28763a46f9d7e6e70718eb47737e1b
vinay <147977755+vinayj16@users.noreply.github.com>
2025-05-18 14:52:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -182,7 +182,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-=======
+
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
